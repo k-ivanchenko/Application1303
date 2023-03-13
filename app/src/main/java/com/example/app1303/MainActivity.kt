9a9f0d9c9e881577ac530ffebe51a01b28a76209
Kseniia <xeniya.ivanchenko@gmail.com>
2023-03-13 11:08:23 +0200

    Added button

--- app/src/main/java/com/example/app1303/MainActivity.kt
+++ app/src/main/java/com/example/app1303/MainActivity.kt
@@ -9,5 +9,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         //TODO user:implement button event handling
+        //TODO bind button
     }
 }
